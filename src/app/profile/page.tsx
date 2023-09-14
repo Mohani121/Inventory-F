77ae63ad968550fa0430258132479790a27da8ff
Mohani121 <mohaworks01@gmail.com>
2023-09-14 17:49:11 +0300

    Logout Completed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { NextResponse } from "next/server";
+import toast from "react-hot-toast";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout Successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div>
       <h1>Profile page</h1>
+      <button onClick={logout} className="py-2 px-4 bg-btn1 rounded-full">
+        Logout
+      </button>
     </div>
   );
 }
